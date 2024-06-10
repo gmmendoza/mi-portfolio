95632563dee3760292dfa1832651c8662a38887c
GuadaMendoza <104594974+gmmendoza@users.noreply.github.com>
2024-06-10 10:04:46 -0300

    Portfolio armado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// js/script.js
+
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Mensaje enviado con éxito');
-    // Aquí puedes agregar más lógica de envío de formulario
+    
 });
